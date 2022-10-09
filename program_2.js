e586f7b6ffdc2933726b392b771fbbac2bb54219
aarti <aarti.gholam@gmail.com>
2022-10-09 20:05:19 +0530

    demo2

---
+++ program_2.js
@@ -0,0 +1,22 @@
+
+// Program 2- Write a program to subtract 2 numbers and print the result on the console.
+
+let a = 100
+let b = 50
+
+let result = a - b
+
+console.log(result);
+
+// Ans : 50
+
+//------------------------------------------------------------------------------------------------//
+
+let a1 = 50
+let b1 = 80
+
+let result1 = a1 - b1
+
+console.log(result1)
+
+// Ans : -30
