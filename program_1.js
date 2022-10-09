1f34b21c65725b4394ac14cce4b2afa20c979077
aarti <aarti.gholam@gmail.com>
2022-10-09 19:53:24 +0530

    demo commit

---
+++ program_1.js
@@ -0,0 +1,22 @@
+
+// Program 1- Write a program to add 2 numbers and print the result on the console.
+
+let a = 10
+let b = 30
+
+let result = a + b
+
+console.log(result);
+
+// Ans : 40
+
+//------------------------------------------------------------------------------------------//
+
+let a1 = -100
+let b1 = 50
+
+let result1 = a1 + b1
+
+console.log(result1);
+
+// Ans : -50
